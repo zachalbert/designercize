d9dccee81c45b759cf852c96f14ab3959c1f8697
zachalbert <zachalbert@gmail.com>
2017-10-10 01:06:02 -0700

    Try to get timer working. Too sleepy.

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -122,23 +122,6 @@ $(document).ready( function() {
 
 });
 
-/*
-** This assumes any option that already exists in a select dropdown is fair game.
-**
-function roll() {
-  var prompts = $('.prompt-selection').toArray();
-
-  // Set a random non-first option in each dropdown
-  $(prompts).each( function( index, prompt ) {
-    var options      = $(prompt).find('option'),
-        randomOption = Math.floor( Math.random() * (options.length-1) ) + 1;
-
-    options.selected = false;
-    options[randomOption].selected = true;
-  });
-}
-*/
-
 function roll( difficulty ) {
   var features = [],
       useCases = [],
@@ -232,5 +215,48 @@ function roll( difficulty ) {
   $('#device').text(randomDevice);
   $('#need').text(randomNeed);
 
-  // start a timer...
+  // var deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+  var countdown = $('#timer-selection').val();
+  console.log(countdown);
+  initializeClock('timer', countdown);
+
+}
+
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    daysSpan.innerHTML = t.days;
+    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
 }
